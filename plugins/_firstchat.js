1247f1c677803c0a36e2e8e20b4a47fb7d3cbd75
Shifna-rahees <102541562+Shifna-rahees@users.noreply.github.com>
2022-03-31 22:05:13 +0530

    Update _firstchat.js

--- plugins/_firstchat.js
+++ plugins/_firstchat.js
@@ -9,7 +9,7 @@ handler.all = async function (m) {
 Hai, ${this.ucapan()}
 
 ${user.banned ? 'kamu dibanned' : `Ada yang bisa ${this.user.name} bantu?`}
-`.trim(), 'ɴᴀɴᴅʜᴜᴛᴛʏ ᴠ3', user.banned ? 'Pemilik Bot' : 'Menu', user.banned ? ',owner' : ',?', m)
+`.trim(), '𝙍𝘼𝙃𝙀𝙀𝙎', user.banned ? 'Pemilik Bot' : 'Menu', user.banned ? ',owner' : ',?', m)
     user.pc = new Date * 1
 }
 
